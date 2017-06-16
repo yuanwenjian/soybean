aa8d30162a8d126cafbefca8938dd1b4ff13e8b8
bmk <yuanwj@biomarker.com.cn>
2017-06-16 17:04:32 +0800

    添加测试文件

---
+++ test/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.4
+# coding=utf-8
+''' test'''
+class test():
+    def __init__(self):
+        self.name="yuanwj"
+
+    @property
+    def desc(self):
+        print(self.name)
+yuanwj=test()
